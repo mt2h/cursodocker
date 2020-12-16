83e52bd69286b90b03995e27605532430f20f096
Cristhián Monasterio <cristhian.monasterio@gmail.com>
2020-12-16 01:23:30 -0300

    add files course of docker complete

---
+++ unidad2/05.dockerfile/04.variables_entorno/loadData.sql
@@ -0,0 +1,11 @@
+use curso;
+
+CREATE TABLE Person (
+	id int,
+	name varchar(256),
+	lastname varchar(256)
+);
+
+INSERT INTO Person VALUES (1, 'Person 1', 'Last Name 1');
+INSERT INTO Person VALUES (1, 'Person 2', 'Last Name 2');
+INSERT INTO Person VALUES (1, 'Person 3', 'Last Name 3');
